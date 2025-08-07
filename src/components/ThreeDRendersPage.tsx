de2d54ae3e12d7ac61a7891177e2d6c9c9688809
superwealthoffice-collab <superwealthoffice@gmail.com>
2025-08-07 15:06:08 +0700

    Fix Export and Share Buttons in Floor Plan and 3D Render Pages

--- src/components/ThreeDRendersPage.tsx
+++ src/components/ThreeDRendersPage.tsx
@@ -374,7 +374,6 @@ const ThreeDRendersPage: React.FC = () => {
         setSelectedLighting={setSelectedLighting}
       />
       <ExportModal isOpen={showExport} onClose={() => setShowExport(false)} />
-      <ExportModal isOpen={showExport} onClose={() => setShowExport(false)} exportType="chat" />
       <DeleteConfirmationModal
         isOpen={showDelete}
         onClose={() => setShowDelete(false)}
